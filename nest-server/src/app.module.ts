22a161ddc259b23b5820f3c2d41829e738b4a9a4
Rajat Verma <cserajatverma@gmail.com>
2026-03-21 21:49:17 +0530

    Add config, MongoDB and auth/users modules
    Register ConfigModule globally and configure MongooseModule with an async factory using ConfigService to read MONGODB_URI. Also add AuthModule, UsersModule and DatabaseModule to AppModule imports to wire up authentication, user management and database integration.

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.getOrThrow<string>('MONGODB_URI'),
+      }),
+    }),
+    AuthModule,
+    UsersModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
